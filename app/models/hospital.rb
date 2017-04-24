9cb0b5fe59ee58b32e1d031a1f9862265b10db1b
Shawn <shawn.nigel@gmail.com>
2017-04-24 23:47:45 +0530

    fix3

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -2,7 +2,8 @@ class Hospital < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
+         #:validatable
   
   after_create :create_tenant
 
