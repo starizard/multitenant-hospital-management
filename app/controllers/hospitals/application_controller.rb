5c41abab7824dcd30178fb2d5555907b804b893a
Shawn <shawn.nigel@gmail.com>
2017-04-24 22:37:27 +0530

    authentication error fixed

--- app/controllers/hospitals/application_controller.rb
+++ app/controllers/hospitals/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base    
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :authenticate_hospital!
+  
   protected
 
   def configure_permitted_parameters
