b8940aee9b102653d2009d11643e85b2a16d5e29
Shawn <shawn.nigel@gmail.com>
2017-04-24 22:21:29 +0530

    minor bug fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   def home
   	@person = current_person 
   	@hname = request.base_url.split('//').second.split('.').first
-    @root = 'lvh'
+    @root = 'muten'
 
   end
 
@@ -16,7 +16,7 @@ class StaticPagesController < ApplicationController
 
   def go_to_subdomain
   	if hospital_signed_in?
-  		@curr_url = "http://"+current_hospital.subdomain+".lvh.me:3000"
+  		@curr_url = "http://"+current_hospital.subdomain+".muten.org"
       redirect_to @curr_url 
 		end
     
