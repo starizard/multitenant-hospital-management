0690915c0e4044667f84b5e189178e669216f2ea
Shawn <shawn.nigel@gmail.com>
2017-04-25 11:54:00 +0530

    fix4

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -2,14 +2,14 @@ class Hospital < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
-         #:validatable
+         :recoverable, :rememberable, :trackable, :validatable
   
   after_create :create_tenant
 
   private
   
-  def create_tenant       
+  def create_tenant
+  		 puts "SUBDOMAIN IS:"+subdomain.to_s+" .."    
          Apartment::Tenant.create(subdomain)
   end
 
